9842f58b6fd77ca4d446d1774d6287afc7f18970
hirarira <hirarira617@gmail.com>
2021-02-06 18:56:39 +0900

    Gridレイアウトが使えるかテスト

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Grid } from '@material-ui/core';
 
 const Index: React.FC = (()=>{
-    return (
-        <div>
-            test
-        </div>
-    )
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        menu
+      </Grid>
+      <Grid item xs={12}>
+        視聴したアニメリスト
+      </Grid>
+    </Grid>
+  )
 });
 
 export default Index;
