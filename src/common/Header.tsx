9d7467403c4e6d456f42f941d438e508d50d2da7
hirarira <hirarira617@gmail.com>
2021-02-07 02:02:52 +0900

    material-uiをinstallしました

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { AppBar, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Header: React.FC = (()=>{
   return (
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
         </IconButton>
         <Typography variant="h6">
           Menu
