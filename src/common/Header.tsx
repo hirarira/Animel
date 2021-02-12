7920857b10ed5eb480bfa8a2aa088ace8e00aa79
hirarira <hirarira617@gmail.com>
2021-02-13 04:25:28 +0900

    Menuを追加しました

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,12 +1,26 @@
-import React from "react";
-import { AppBar, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
+import React, { useState } from "react";
+import { AppBar, Grid, IconButton, makeStyles, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
+const useStyles = makeStyles((theme) => ({
+  section: {
+  },
+}));
+
 const Header: React.FC = (()=>{
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const classes = useStyles();
   return (
+    <>
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
+        <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleClick}>
           <MenuIcon />
         </IconButton>
         <Typography variant="h6">
@@ -14,6 +28,19 @@ const Header: React.FC = (()=>{
         </Typography>
       </Toolbar>
     </AppBar>
+    <Menu
+      id="menu"
+      anchorEl={anchorEl}
+      keepMounted
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+      className={classes.section}
+    >
+      <MenuItem>Profile</MenuItem>
+      <MenuItem>My account</MenuItem>
+      <MenuItem>Logout</MenuItem>
+    </Menu>
+    </>
   )
 });
 
