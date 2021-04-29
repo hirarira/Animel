3ec11fc0eca06d139b16b644f36abce6bc1e9e7f
hirarira <hirarira617@gmail.com>
2021-04-29 17:20:38 +0900

    見逃しアニメトップページの雛形を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import WatchAnimeList from './pages/WatchAnimeList/Index';
+import ShowMinogashiAnimeTop from './pages/ShowMinogashiAnime/Index';
 
 function App() {
   return (
     <div className="App">
       <Router basename={'/Animel'}>
         <Route exact path='/' component={WatchAnimeList}/>
+        <Route exact path='/showMinogashiAnime' component={ShowMinogashiAnimeTop}/>
       </Router>
     </div>
   );
---
+++ src/pages/ShowMinogashiAnime/Index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Header from "../../common/Header";
+
+const ShowMinogashiAnimeTop: React.FC = (()=>{
+    return (
+      <>
+        <Header
+          isPrivate={true}
+        />
+        test
+      </>
+    )
+});
+
+export default ShowMinogashiAnimeTop;
